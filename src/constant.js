c1c4ab65d4d17ccf808d223a48363c4b63d7573e
banzhenyu <banzhenyu@yeah.net>
2020-06-03 16:22:51 +0800

    upd: username

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 export default {
-    GITHUB_PROJECT_INFO: 'https://api.github.com/repos/yzb0101/genmd',
-    // GITHUB_PROJECT_INFO: 'http://127.0.0.1:3000/repos/yzb0101/genmd',
-    GITHUB_PROJECT_URL: 'https://www.github.com/yzb0101/genmd',
-    GITHUB_VIDEO_URL: '-' || 'https://github.com/yzb0101/genmd/raw/master/pic/genmd.mp4',
+    GITHUB_PROJECT_INFO: 'https://api.github.com/repos/ewxm/genmd',
+    // GITHUB_PROJECT_INFO: 'http://127.0.0.1:3000/repos/ewxm/genmd',
+    GITHUB_PROJECT_URL: 'https://www.github.com/ewxm/genmd',
+    GITHUB_VIDEO_URL: '-' || 'https://github.com/ewxm/genmd/raw/master/pic/genmd.mp4',
 }
